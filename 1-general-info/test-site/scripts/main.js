3b831eaeb3d997f00cd013494c1c3ce70eb9ca67
Krzysztof <krzysztofbem@gmail.com>
2022-07-26 21:31:13 +0200

    js updated

--- 1-general-info/test-site/scripts/main.js
+++ 1-general-info/test-site/scripts/main.js
@@ -1,25 +1,39 @@
-var myHeading = document.querySelector('h1')
-myHeading.textContent = 'Hello world!'
+// var myHeading = document.querySelector('h1')
+// myHeading.textContent = 'Hello world!'
 
-const name = 'Zygmuś'
-console.log(name)
+// const name = 'Zygmuś'
+// console.log(name)
 
-const age = 1666
-console.log(name, age)
+// const age = 1666
+// console.log(name, age)
 
-console.log(age)
+// console.log(age)
 
-const favouriteDish = 'kotlet z narzekaniem'
+// const favouriteDish = 'kotlet z narzekaniem'
 
-console.log(name, age, favouriteDish)
+// console.log(name, age, favouriteDish)
 
-console.log(`Cześć, jestem ${name} i moim ulubionym daniem jest ${favouriteDish}`)
+// console.log(`Cześć, jestem ${name} i moim ulubionym daniem jest ${favouriteDish}`)
 
-const myName = 'Alojzy'
-const petName = 'Łajka'
+// const myName = 'Alojzy'
+// const petName = 'Łajka'
 
-console.log(`Cześć, jestem ${myName}, a to jest ${petName} - mój bąk`)
+// console.log(`Cześć, jestem ${myName}, a to jest ${petName} - mój bąk`)
 
-const petAge = 99
+// const petAge = 99
 
-console.log('' + petName + ' ma już ' + petAge + ' lat. ')
+// console.log('' + petName + ' ma już ' + petAge + ' lat. ')
+
+const firstMaleName = 'Tomek'
+let age = 12
+age = 18
+
+const favColor = 'niebieski'
+const favMmeal = 'schabowy'
+
+let currentCar
+currentCar = 'Audi'
+
+let firstFemaleName = 'Ania'
+let age2 = 24
+let favColor2 = 'czerwony'
