6149e28cb39765feb48346a8946da893c21bb55d
Krzysztof <krzysztofbem@gmail.com>
2022-07-28 20:58:56 +0200

    js updated

--- 1-general-info/test-site/scripts/main.js
+++ 1-general-info/test-site/scripts/main.js
@@ -40,3 +40,18 @@ let favColor2 = 'czerwony'
 
 const quote = 'Jegomość powiedział:" kocham ser z żurawiną " '
 console.log(quote)
+
+console.log('Witaj świecie')
+
+let modified = 12
+console.log(typeof modified)
+const text = 'Język java script'
+console.log(text)
+console.log(typeof text)
+
+let signature = 'JavaScript to język programowania'
+console.log(`Informacja: ${signature} `)
+
+let a = 21
+let b = 17
+console.log(alert(a + b))
